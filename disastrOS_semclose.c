09693e1629ca627d5d72fe78944097970cbb6eb6
Daniele <daniele.michelori@gmail.com>
2019-09-23 18:07:31 +0200

    Bozza semclose
    Verifica presenza file descriptor ed eventuale rimozione

--- disastrOS_semclose.c
+++ disastrOS_semclose.c
@@ -8,4 +8,18 @@
 
 void internal_semClose(){
   // do stuff :)
+  int fd = running->syscall_args[0];  //assegnazione file descriptor processo corrente
+
+  //Descriptor*  DescriptorList_byFd(ListHead* l, int fd)
+  SemDescriptor* fd_sem = SemDescriptorList_byFd(&running->sem_descriptors, fd);  //verifico che il descrittore fd si trovi nella lista dei descrittori del semaforo
+
+  if(!fd_sem) {  //caso in cui il descittore fd non è presente
+    printf("ERROR: il file descriptor cercato: %d non è presente\n", fd);
+    return;
+  }
+  printf("Il file descriptor cercato: %d esiste!\n", fd);
+
+  printf("Rimozione del descriptor: %d associato al semaforo con id: %d\n\n", fd, fd_sem->semaphore->id);
+  //ListItem* List_detach(ListHead* head, ListItem* item);
+  fd_sem = List_detach(&running->sem_descriptors, fd_sem);  //rimuovo il fd dalla lista dei descrittori del processo
 }
