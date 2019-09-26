a3eb199410639254b29958b17372c32fdbd77e11
Daniele <daniele.michelori@gmail.com>
2019-09-26 13:09:11 +0200

    Continuo semwait
    Spostamento del puntatore a descrittore nella lista di Waiting

--- disastrOS_semwait.c
+++ disastrOS_semwait.c
@@ -13,15 +13,18 @@ void internal_semWait(){
   SemDescriptor* fd_sem = SemDescriptorList_byFd(&running->sem_descriptors, fd);  //verifico la presenza del descrittore nella lista dei descittori del semaforo
   Semaphore* sem = fd_sem->semaphore;  //risalgo al semaforo del descrittore: fd_sem
 
-  if(sem->count >= 0) {
-    printf("Il contatore del semaforo con id: %d ha valore: %d\n", sem->id, sem->count);
-  }
-
+  printf("[WAIT] Il contatore del semaforo con id: %d passa dal valore: %d al valore: %d\n", sem->id, sem->count, sem->count-1);
   sem->count = sem->count - 1;  //decremento contatore Semaphore associato alla Wait
+  SemDescriptorPtr* fd_semPtr = fd_sem->ptr;
 
+  printf("Sposto il puntatore a descrittore nella lista di Waiting...\n");
   if(sem->count < 0) {  //il contatore del semaforo ha valore negativo -> sospendo il task
-    printf("Il contatore del semaforo con id: %d ha valore negativo: %d\n", sem->id, sem->count);
+    SemDescriptorPtr* ret = List_detach(&sem->descriptors, (ListItem *) fd_semPtr);  //rimuovo il puntatore a descrittore dalla lista dei descittori del semaforo
+    ret = List_insert(&sem->waiting_descriptors, sem->waiting_descriptors.last, (ListItem *) fd_semPtr);  //inserisco il puntatore a descrittore nella lista di waiting del semaforo
   }
+
+
+
 
 
 }
